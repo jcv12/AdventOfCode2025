06ab274a34c6a8a381170ec6610cf6ee5d253eec
JCV12 <john.c.vayianos@gmail.com>
2025-12-05 12:49:29 -0500

    I took a nap and forgot about this, anyway I reformattde the code into a class Program then moved the code into functions. aLSO added the FileRead stuff so we can try an example file

--- AdventOfCode2025Day1/Program.cs
+++ AdventOfCode2025Day1/Program.cs
@@ -1,18 +1,126 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
 
-int startingLocation = 0;
-int directionTravel = 1;
-int result = 0;
-int finalLocation = 0;
 
-result = startingLocation - directionTravel;
-
-Console.WriteLine(result);
-
-if (result < 0)
+class Program
 {
-    Console.WriteLine("This is negative so we need to reset to 100");
-    finalLocation = 100 + result;
-    Console.WriteLine($"This click was: {finalLocation}");
+    static void Main()
+    {
+        string filePath = "ExampleInput.txt";
+
+        if (!File.Exists(filePath))
+        {
+            Console.WriteLine("File not found!");
+            return;
+        }
+
+
+        //we found trhe file yay
+
+        foreach(string line in File.ReadLines(filePath))
+        {
+            parseInput(line);
+        }
+    }
+
+
+
+
+
+    static void parseInput(string inputValue)
+    {
+        char firstCharacter = inputValue[0]; //Grab the first character, L means Left, R means Right
+        string stringNumber = inputValue.Substring(1); //Grab the number of clicks they want us to move
+
+        Console.WriteLine(inputValue);
+
+        if (firstCharacter == 'L')
+        {
+            moveLeft(int.Parse(stringNumber));
+        }
+
+
+        if (firstCharacter == 'R')
+        {
+            moveRight(int.Parse(stringNumber));
+        }
+
+
+    }
+
+
+    static void moveLeft(int travelDistance)
+    {
+        Console.WriteLine("Moving Left: " + travelDistance);
+    }
+
+
+    static void moveRight(int travelDistance)
+    {
+        Console.WriteLine("Moving Right: " + travelDistance);
+    }
+
+
+
 }
+
+
+
+
+
+//int startingLocation = 50;
+//int directionTravel = 68;
+//int result = 0;
+//int finalLocation = 0;
+
+//result = startingLocation - directionTravel;
+
+//Console.WriteLine(result);
+
+//if (result < 0)
+//{
+//    Console.WriteLine("This is negative so we need to reset to 100");
+//    finalLocation = 100 + result;
+//    Console.WriteLine($"This click was: {finalLocation}");
+//}
+
+
+//parseInput("L68");
+
+
+
+
+//static void parseInput(string inputValue)
+//{
+//    char firstCharacter = inputValue[0]; //Grab the first character, L means Left, R means Right
+//    string stringNumber = inputValue.Substring(1); //Grab the number of clicks they want us to move
+
+//    Console.WriteLine(inputValue);
+
+//    if (firstCharacter == 'L')
+//    {
+//        moveLeft(int.Parse(stringNumber));
+//    }
+
+
+//    if (firstCharacter == 'R')
+//    {
+//        moveRight(int.Parse(stringNumber));
+//    }
+
+
+//}
+
+
+//static void moveLeft(int travelDistance)
+//{
+//    Console.WriteLine("Moving Left: " + travelDistance);
+//}
+
+
+//static void moveRight(int travelDistance)
+//{
+//    Console.WriteLine("Moving Right: " + travelDistance);
+//}
+
+
--- AdventOfCode2025Day1/obj/Debug/net8.0/AdventOfCode2025Day1.AssemblyInfo.cs
+++ AdventOfCode2025Day1/obj/Debug/net8.0/AdventOfCode2025Day1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdventOfCode2025Day1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2117b26a823bc3751dd011a4b075a52d2534204")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f5d71718222aea2dc0484e014defd52c693fc72")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdventOfCode2025Day1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdventOfCode2025Day1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
