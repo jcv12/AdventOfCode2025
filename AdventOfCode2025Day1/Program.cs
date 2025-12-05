b332cd143816cbc4f7a76184acfc9a2b98551ce0
JCV12 <john.c.vayianos@gmail.com>
2025-12-05 16:38:01 -0500

    Fixed bug of more than 200 int, cleaned up right and left conditionals

--- AdventOfCode2025Day1/Program.cs
+++ AdventOfCode2025Day1/Program.cs
@@ -52,7 +52,7 @@ class Program
             if (finalDestination < 0)
             {
                 //Console.WriteLine("This is negative so we need to reset to 100");
-                finalDestination = 100 + finalDestination;
+                finalDestination = (finalDestination % 100 + 100) % 100;
 
             }
 
@@ -67,10 +67,13 @@ class Program
 
             finalDestination = currentLocation + int.Parse(stringNumber); // Moving to the right
 
+            //found an edge case where the data provides a number like 438 and what this really means is it went into 4 complete rotations so we should really treat this like 48
+
             if (finalDestination > 99)
             {
+                finalDestination = finalDestination % 100;
                 //Console.WriteLine("This is over 99 so we need to reset to the bottom numbers");
-                finalDestination = finalDestination - 100;
+                //finalDestination = finalDestination - 100;
             }
             
 
@@ -87,64 +90,3 @@ class Program
 
 
 }
-
-
-
-
-
-//int startingLocation = 50;
-//int directionTravel = 68;
-//int result = 0;
-//int finalLocation = 0;
-
-//result = startingLocation - directionTravel;
-
-//Console.WriteLine(result);
-
-//if (result < 0)
-//{
-//    Console.WriteLine("This is negative so we need to reset to 100");
-//    finalLocation = 100 + result;
-//    Console.WriteLine($"This click was: {finalLocation}");
-//}
-
-
-//parseInput("L68");
-
-
-
-
-//static void parseInput(string inputValue)
-//{
-//    char firstCharacter = inputValue[0]; //Grab the first character, L means Left, R means Right
-//    string stringNumber = inputValue.Substring(1); //Grab the number of clicks they want us to move
-
-//    Console.WriteLine(inputValue);
-
-//    if (firstCharacter == 'L')
-//    {
-//        moveLeft(int.Parse(stringNumber));
-//    }
-
-
-//    if (firstCharacter == 'R')
-//    {
-//        moveRight(int.Parse(stringNumber));
-//    }
-
-
-//}
-
-
-//static void moveLeft(int travelDistance)
-//{
-//    Console.WriteLine("Moving Left: " + travelDistance);
-//}
-
-
-//static void moveRight(int travelDistance)
-//{
-//    Console.WriteLine("Moving Right: " + travelDistance);
-//}
-
-
--- AdventOfCode2025Day1/obj/Debug/net8.0/AdventOfCode2025Day1.AssemblyInfo.cs
+++ AdventOfCode2025Day1/obj/Debug/net8.0/AdventOfCode2025Day1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdventOfCode2025Day1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e913a8538a31880dd7e6de012df29a647ae7b881")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cb455f7048c52c57964d9142c9b6638ab56a43d")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdventOfCode2025Day1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdventOfCode2025Day1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
